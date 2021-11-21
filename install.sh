2a45a44ffd6f56289056c57b00090b7ddcaf282f
Gabriel Caussi <gcaussi@gmail.com>
2021-11-20 22:27:38 -0300

    Add screenshots

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Project     : Fedora Workstation
-# Description : Workstation running Fedora Linux
+# Project     : Fedora KDE Plasma
+# Description : Workstation running Fedora Linux KDE Plasma
 # Developer   : Gabriel Caussi
 # Github      : https://github.com/gabrielcaussi
 # Linkedin    : https://www.linkedin.com/in/gabrielcaussi
@@ -10,11 +10,10 @@
 sudo dnf update -y
 
 ## Install wifi module for Thinkpad E431 (BCM43142)
-##sudo apt install broadcom-sta-dkms -y
 sudo dnf install broadcom-wl -y
 
 ## Install dependencies and softwares
-sudo dnf install g++ gcc util-linux ffmpeg wget curl git neofetch htop zsh tmux neovim hugo flameshot -y
+sudo dnf install g++ gcc util-linux ffmpeg wget curl git neofetch htop zsh tmux neovim hugo -y
 
 ## Install Google Chrome
 ##wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
@@ -26,18 +25,15 @@ sudo dnf install g++ gcc util-linux ffmpeg wget curl git neofetch htop zsh tmux 
 # sudo apt install ./code_1.61.2-1634656828_amd64.deb
 # rm code_1.61.2-1634656828_amd64.deb
 
-## Create directory ~/.config
-mkdir ~/.config
-
 ## Git
-ln -sf ~/dotfiles/.gitconfig ~/
+ln -sf ~/.dotfiles/.gitconfig ~/
 
 ## Tmux
-ln -sf ~/dotfiles/.tmux.conf ~/
+ln -sf ~/.dotfiles/.tmux.conf ~/
 
 ## Fonts
-ln -sf ~/dotfiles/.fonts ~/
+ln -sf ~/.dotfiles/.fonts ~/
 
 ## Plasma setup
 plasma-apply-lookandfeel -a org.kde.breezedark.desktop
-plasma-apply-wallpaperimage ~/dotfiles/wallpaper.png
+plasma-apply-wallpaperimage ~/.dotfiles/wallpaper.png
